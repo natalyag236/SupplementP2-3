d9df7def11830192b5984c15597ee66633a449e2
Natalya Goldsmith <ngol1755@students.vsu.edu>
2024-11-30 21:17:50 -0500

    doc: add documentation to binary search

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,12 +1,22 @@
-
+/**
+ * This class implements the binary search algorithm to find a target element
+ * in a sorted array.
+ */
 public class BinarySearch {
     
+    /**
+     * Performs binary search on a sorted array to find the index of the target element.
+     *
+     * @param sortedList The sorted array to search through.
+     * @param target The element to search for.
+     * @return The index of the target element if found, or -1 if the element is not found.
+     */
     public static int binarySearch(int[] sortedList, int target) {
         int left = 0;
         int right = sortedList.length - 1;
 
         while (left <= right) {
-            int mid = left + (right - left) / 2;
+            int mid = left + (right - left) / 2;   
 
             if (sortedList[mid] == target) {
                 return mid; 
@@ -19,6 +29,7 @@ public class BinarySearch {
                 left = mid + 1;
             }
         }
+        
         return -1;
     }
 }
