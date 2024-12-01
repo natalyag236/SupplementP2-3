29481cbe5c36bec58d33a27168a18b9fdc40d4fb
Natalya Goldsmith <ngol1755@students.vsu.edu>
2024-11-30 21:44:10 -0500

    test: updated  test for  matrix utils

--- MatrixUtilsTest.java
+++ MatrixUtilsTest.java
@@ -14,13 +14,13 @@ public class MatrixUtilsTest {
     public void testDeterminant3x3Matrix() {
         double[][] matrix = {{2, 5, 8}, {1, 3, 4}, {7, 6, 9}};
         double result = MatrixUtils.determinant(matrix);
-        assertEquals("Determinant of 3x3 matrix", 12.0, result, 0.001);
+        assertEquals("Determinant of 3x3 matrix", -19.0, result, 0.001);
     }
 
     @Test
     public void testDeterminant4x4Matrix() {
         double[][] matrix = {{3, 2, 1, 4}, {0, 5, 6, 7}, {8, 9, 10, 11}, {12, 13, 14, 15}};
         double result = MatrixUtils.determinant(matrix);
-        assertEquals("Determinant of 4x4 matrix", -204.0, result, 0.001);
+        assertEquals("Determinant of 4x4 matrix", -64.0, result, 0.001);
     }
 }
