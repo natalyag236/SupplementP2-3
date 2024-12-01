ab910b09cf0b8d99bbee9b83ec79f6c71ee2bfbd
Natalya Goldsmith <ngol1755@students.vsu.edu>
2024-11-30 21:56:50 -0500

    doc: add documentation to matrixutils

--- MatrixUtils.java
+++ MatrixUtils.java
@@ -1,10 +1,29 @@
+/**
+ * MatrixUtils provides static methods to perform matrix operations.
+ * Specifically, it provides functionality to calculate the determinant of a square matrix.
+ */
 public class MatrixUtils {
 
+    /**
+     * This method calculates the determinant of a square matrix.
+     *
+     * @param mat The input square matrix (2D array).
+     * @return The determinant of the matrix.
+     */
     static double determinant(double[][] mat) {
-        int n = mat.length;
-        return determinantOfMatrix(mat, n);
+        int n = mat.length;  
+        return determinantOfMatrix(mat, n); 
     }
 
+    /**
+     * This method generates the cofactor (minor) matrix by excluding the specified row and column.
+     *
+     * @param mat The input matrix.
+     * @param temp The temporary matrix to store the minor matrix.
+     * @param p The row index to exclude.
+     * @param q The column index to exclude.
+     * @param n The current size of the matrix.
+     */
     static void getCofactor(double mat[][], double temp[][], int p, int q, int n) {
         int i = 0, j = 0;
 
@@ -21,15 +40,21 @@ public class MatrixUtils {
         }
     }
 
+    /**
+     * This recursive method calculates the determinant of a matrix using cofactor expansion.
+     *
+     * @param mat The input matrix.
+     * @param n The size of the matrix.
+     * @return The determinant of the matrix.
+     */
     static double determinantOfMatrix(double mat[][], int n) {
-        double D = 0; 
+        double D = 0;  
 
         if (n == 1)
             return mat[0][0];
 
-        double temp[][] = new double[n][n];
-        double sign = 1;
-
+        double temp[][] = new double[n][n];  
+        double sign = 1;  
         for (int f = 0; f < n; f++) {
             getCofactor(mat, temp, 0, f, n);
             D += sign * mat[0][f] * determinantOfMatrix(temp, n - 1);
@@ -37,6 +62,6 @@ public class MatrixUtils {
             sign = -sign;
         }
 
-        return D;
+        return D;  // Return the final computed determinant.
     }
 }
